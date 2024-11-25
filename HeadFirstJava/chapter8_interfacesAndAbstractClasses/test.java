184e9673f731701636914d5d38ddbd3ad44152a1
kvreinzapp <klausvonreinherzapp@gmail.com>
2024-11-25 21:49:03 +0800

    a little step on java

--- HeadFirstJava/chapter8_interfacesAndAbstractClasses/test.java
+++ HeadFirstJava/chapter8_interfacesAndAbstractClasses/test.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Animal {
   void fight() {
     System.out.println("i am fighting.");
@@ -12,7 +14,19 @@ class dog extends Animal {
 
 public class test {
   public static void main(String[] args) {
-    Object o = new dog();
-    System.out.println(o.getClass());
+    ArrayList<Animal> list = new ArrayList<Animal>();
+    dog aDog = new dog();
+    list.add(aDog);
+    dog d = list.get(0);
   }
+}
+
+
+public Object getObject(Object o){
+	return o;
+}
+
+public void go(){
+	Dog aDog = new Dog();
+	Dog sameDog = getObject(aDog);
 }

7229e3c444adad14de18e35f3ee7ff91e3ae86cf
kvreinzapp <klausvonreinherzapp@gmail.com>
2024-12-26 15:45:09 +0800

    new gui file

---
+++ UCSanDiegoJava/eclipse-workspace/UCSDUnfoldingMaps/src/guimodule/MyDisplay.java
@@ -0,0 +1,13 @@
+package guimodule;
+
+import processing.core.PApplet;
+
+public class MyDisplay extends PApplet{
+	public void setup() {
+		size(400, 400);
+		background(255,200,200);
+	}
+	public void draw() {
+		
+	}
+}
